be39147918fa077abd49139fee510b2ff8d003be
Nordahl83 <andreas.nordahl.sp15@thegameassembly.com>
2016-01-15 18:45:10 +0100

    Updated inventory and item files

--- Source/Game/Item.cpp
+++ Source/Game/Item.cpp
@@ -5,25 +5,55 @@
 Item::Item()
 {
 	mySprite = nullptr;
+	myCombinableWithList.Init(4);
 
 	myName = "";
 	myDescription = "";
+	myLevelToSpawnIn = "";
 
 	myIsCombinable = false;
 	myPickupStatus = false;
+	myIsClicked = false;
 
 	myPosition = DX2D::Vector2f(0.0f, 0.0f);
 }
 
-Item::Item(DX2D::CSprite* aSprite, std::string& aItemName, std::string& aItemDescription,
-	DX2D::Vector2f& aPosition, bool aCombinableStatus, bool aCanBePickedUpStatus)
+//Constructor that sets all of the items values except the sprite
+Item::Item(std::string& aItemName, std::string& aItemDescription,
+	DX2D::Vector2f& aPosition, bool aCombinableStatus, bool aCanBePickedUpStatus,
+	std::string& aLevelToSpawnIn)
 {
-	mySprite = aSprite;
+	myCombinableWithList.Init(4);
+
 	myName = aItemName;
 	myDescription = aItemDescription;
+	myLevelToSpawnIn = aLevelToSpawnIn;
+
 	myPosition = aPosition;
+	
 	myIsCombinable = aCombinableStatus;
 	myPickupStatus = aCanBePickedUpStatus;
+	myIsClicked = false;
+}
+
+//Constructor that sets all of the items values
+Item::Item(DX2D::CSprite* aSprite, std::string& aItemName, std::string& aItemDescription,
+	DX2D::Vector2f& aPosition, bool aCombinableStatus, bool aCanBePickedUpStatus,
+	std::string& aLevelToSpawnIn)
+{
+	myCombinableWithList.Init(4);
+
+	mySprite = aSprite;
+
+	myName = aItemName;
+	myDescription = aItemDescription;
+	myLevelToSpawnIn = aLevelToSpawnIn;
+
+	myPosition = aPosition;
+
+	myIsCombinable = aCombinableStatus;
+	myPickupStatus = aCanBePickedUpStatus;
+	myIsClicked = false;
 }
 
 Item::~Item()
@@ -31,34 +61,34 @@ Item::~Item()
 	SAFE_DELETE(mySprite);
 }
 
-void Item::SetSprite(DX2D::CSprite* aSprite)
-{
-	mySprite = aSprite;
-}
-
-void Item::SetName(std::string& aName)
-{
-	myName = aName;
-}
-
-void Item::SetDescription(std::string& aDescription)
-{
-	myDescription = aDescription;
-}
-
+//Sets the items position on the screen
 void Item::SetPosition(DX2D::Vector2f& aPosition)
 {
 	myPosition = aPosition;
 }
 
+//Sets if the item can be combined with something else (true / false)
 void Item::SetCombinable(bool aCombinableStatus)
 {
 	myIsCombinable = aCombinableStatus;
 }
 
+//Sets if the item can be picked up (true / false)
 void Item::SetPickupStatus(bool aPickupStatus)
 {
 	myPickupStatus = aPickupStatus;
+}
+
+//Adds the items sprite to the rendering buffer
+void Item::Draw()
+{
+
+}
+
+//Gets the list of item names that this item can be combined with
+CommonUtilities::GrowingArray<std::string>& Item::GetList()
+{
+	return myCombinableWithList;
 }
 
 bool Item::operator ==(const Item& aItem)
