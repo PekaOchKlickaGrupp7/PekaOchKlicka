5934b2b4e796d09633d72eef3bd7e526cf8dbdbe
Erik Jeppsson <napoleonshatt@gmail.com>
2016-01-20 09:43:04 +0100

    Added NavMesh class and Init func in Room

--- Source/Game/Room.cpp
+++ Source/Game/Room.cpp
@@ -8,3 +8,11 @@ Room::Room()
 Room::~Room()
 {
 }
+
+void Room::Init(ObjectData* aObjectData, const CommonUtilities::GrowingArray<Point2f>& aNavPoints)
+{
+	for (unsigned short i = 0; i < aNavPoints.Size(); i++)
+	{
+		myNavMesh.AddPoint(aNavPoints[i]);
+	}
+}
