3ed9fb7f5165fafd18695490b059a5410407fe2f
Linus Neuman <linus@neuman.io>
2016-01-22 15:16:35 +0100

    New event: playsound

---
+++ Source/Game/EventPlaySound.cpp
@@ -0,0 +1,29 @@
+#include "stdafx.h"
+#include "EventPlaySound.h"
+#include "SoundFileHandler.h"
+
+EventPlaySound::EventPlaySound()
+{
+}
+
+bool EventPlaySound::Update(const float aDeltaTime)
+{
+	if (myIs3D)
+	{
+		// not yet
+		// Get3DSound
+		// set looping
+		// set position
+	}
+	else
+	{
+		SoundFileHandler::GetInstance()->GetSound(myTargetSound)->SetLooping(myIsLooping);
+		SoundFileHandler::GetInstance()->GetSound(myTargetSound)->Play();
+	}
+	return true;
+}
+
+
+EventPlaySound::~EventPlaySound()
+{
+}
