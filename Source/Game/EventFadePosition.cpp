d88b914505514eea3c4fa4432526df21046502ae
danneown1 <daniel.nordlund.sp15@thegameassembly.com>
2016-01-26 23:28:27 +0100

    Updated Level Editor.

--- Source/Game/EventFadePosition.cpp
+++ Source/Game/EventFadePosition.cpp
@@ -12,8 +12,40 @@ EventFadePosition::~EventFadePosition()
 
 bool EventFadePosition::Update(const float aDeltaTime)
 {
-	(aDeltaTime);
-	return true;
+	if (myCached == false)
+	{
+		ObjectData* object = GetGameObject(myTarget);
+		if (object != nullptr)
+		{
+			myPrevPosition = { myObjectData->myX, myObjectData->myY };
+		}
+		myCached = true;
+	}
+	myTime += aDeltaTime;
+
+	ObjectData* object = GetGameObject(myTarget);
+	if (object != nullptr)
+	{
+		DX2D::Vector2f pos = {	Lerp(myPrevPosition.x, object->myX + myTargetOffset.x, myTime / myDuration),
+								Lerp(myPrevPosition.y, object->myY + myTargetOffset.y, myTime / myDuration)
+							};
+		myObjectData->myX = pos.x;
+		myObjectData->myY = pos.y;
+		if (myTime >= myDuration)
+		{
+			return true;
+		}
+	}
+	else
+	{
+		return true;
+	}
+	return false;
+}
+
+float EventFadePosition::Lerp(float aFrom, float aTo, float aTime)
+{
+	return aFrom + aTime * (aTo - aFrom);
 }
 
 void EventFadePosition::Reset()
--- Source/Game/EventFadePosition.h
+++ Source/Game/EventFadePosition.h
@@ -10,7 +10,12 @@ public:
 	virtual bool Update(const float aDeltaTime) override;
 	virtual void Reset() override;
 
+	DX2D::Vector2f myTargetOffset;
+	float myDuration;
 protected:
+	float Lerp(float aFrom, float aTo, float aTime);
+
+	DX2D::Vector2f myPrevPosition;
 	float myTime;
 	bool myCached;
 };
