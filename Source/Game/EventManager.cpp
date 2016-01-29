3b2f157885d47841fc3adfdcd3a208ade4953239
Daniel <daniel.nordlund.sp15@thegameassembly.com>
2016-01-29 09:18:50 +0100

    Updated EventManager

--- Source/Game/EventManager.cpp
+++ Source/Game/EventManager.cpp
@@ -129,7 +129,6 @@ void EventManager::Update(const float aDeltaTime)
 	for (unsigned int i = 0; i < (*myObjects).Size(); ++i)
 	{
 		OnEvent((*myObjects)[i], EventTypes::OnHover, mousePosition.x, mousePosition.y, 0, 0);
-		//OnEvent((*myObjects)[i], EventTypes::OnLeave, mousePosition.x, mousePosition.y, 0, 0);
 	}
 
 	for (int i = myActiveEvents.Size() - 1; i >= 0; --i)
@@ -148,6 +147,7 @@ void EventManager::Update(const float aDeltaTime)
 			myActiveEvents.RemoveCyclicAtIndex(i);
 		}
 	}
+
 	if (myIsSwitchingRoom == true)
 	{
 		myIsSwitchingRoom = false;
