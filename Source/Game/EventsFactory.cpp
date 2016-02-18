a5b470bf24fdf14f77f52e95228553eac72caef2
Daniel <daniel.nordlund.sp15@thegameassembly.com>
2016-02-18 14:15:14 +0100

    ...

--- Source/Game/EventsFactory.cpp
+++ Source/Game/EventsFactory.cpp
@@ -565,7 +565,7 @@ Event* EventsFactory::CreateEventData(ObjectData* aData, Value& aParent, Room* a
 			var->myScale = extra["Scale"].GetFloat();
 		}
 
-		var->myFrameDuration = max(0.01f, var->myFrameDuration);
+		var->myFrameDuration = max(0.0001f, var->myFrameDuration);
 		var->myFramesPerRow = max(1, var->myFramesPerRow);
 		var->myNumberOfFrames = max(1, var->myNumberOfFrames);
 
